f8a615d3909c8709171670374547857bbb51c7b0
M ASRAN <62879334+theazran@users.noreply.github.com>
2024-02-18 19:14:52 +0800

    await

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const notificationApiUrl = "https://notifku.my.id/send";
 const notificationClient = new MyNotificationLibrary(notificationApiUrl);
 
 app.get("/", (req, res) => {
-  res.send("Home");
+  res.send("Webhook running...");
 });
 
 app.post("/webhook", (req, res) => {
@@ -18,17 +18,22 @@ app.post("/webhook", (req, res) => {
   console.log(data);
   const message = `*Donasi Baru Diterima 🎉*\n\n*ID*: ${data.id}\n*Tipe*: ${data.type}\n*Jumlah*: Rp ${data.amount_raw}\n*Potongan*: Rp ${data.cut}\n*Nama*: ${data.donator_name}\n*Pesan*: ${data.message}`;
 
-  notificationClient
-    .sendNotification("1234567890",
-      "chat","6285255646434",
-      message,
-    )
-    .then((result) => {
-      console.log(JSON.stringify(result));
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  async function sendNotification() {
+  try {
+    const result = await notificationClient.sendNotification(
+      "1234567890",
+      "chat",
+      "6285255646434",
+      message
+    );
+    console.log(JSON.stringify(result));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the async function
+sendNotification();
 
   res.send("OK");
 });
