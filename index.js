793488d440a506266dfe700abc547e6ebe770b85
M ASRAN <62879334+theazran@users.noreply.github.com>
2024-11-23 01:33:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,16 @@ app.get("/", async (req, res) => {
 app.post("/webhook", async (req, res) => {
   const data = req.body; // Extracting data from the request body
 
-  // Send a WhatsApp message with donation details
-  await sendMessage("6285255646434@s.whatsapp.net", `*Donasi Baru Diterima 🎉*\n\n*ID*: ${data.id}\n*Tipe*: ${data.type}\n*Jumlah*: Rp ${data.amount_raw}\n*Potongan*: Rp ${data.cut}\n*Nama*: ${data.donator_name}\n*Pesan*: ${data.message}`)
-    .then((result) => {
-      console.log(JSON.stringify(result)); // Log the successful result
-    })
-    .catch((error) => {
-      console.error(error); // Log any errors that occur during sending the message
-    });
+  try {
+    const result = await sendMessage("6285255646434@s.whatsapp.net", `*Donasi Baru Diterima 🎉*\n\n*ID*: ${data.id}\n*Tipe*: ${data.type}\n*Jumlah*: Rp ${data.amount_raw}\n*Potongan*: Rp ${data.cut}\n*Nama*: ${data.donator_name}\n*Pesan*: ${data.message}`);
+    console.log(JSON.stringify(result)); // Log the successful result
+  } catch (error) {
+    console.error(error); // Log any errors that occur during sending the message
+  }
 
   res.send("OK"); // Respond with "OK" to acknowledge receipt of the webhook
 });
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
